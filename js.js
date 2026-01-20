e19d7882fac16d55be7668d5733b8cf944aba107
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-01-20 15:06:32 +0100

    finito primo

---
+++ js.js
@@ -0,0 +1,11 @@
+// const inputValue = document.querySelector("#input").value;
+// const save = document.querySelector("#save");
+// const remove = document.querySelector("#delete");
+
+// save.addEventListener("click", () => {
+//   const inputValue = document.querySelector("#input").value;
+//   const text = localStorage.setItem("testo", inputValue);
+// });
+// remove.addEventListener("click", () => {
+//   localStorage.removeItem("testo");
+// });
