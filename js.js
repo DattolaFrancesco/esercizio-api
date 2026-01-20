b04845f1e5cfcbe4a7d02822dabbacf83d83c985
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-01-20 15:41:31 +0100

    homework done

--- js.js
+++ js.js
@@ -9,3 +9,15 @@
 // remove.addEventListener("click", () => {
 //   localStorage.removeItem("testo");
 // });
+window.addEventListener("load", () => {
+  num = sessionStorage.getItem("number");
+  number.textContent = num;
+});
+
+const number = document.querySelector("#number");
+let num = 0;
+setInterval(() => {
+  num++;
+  number.textContent = num;
+  sessionStorage.setItem("number", number.textContent);
+}, 1000);
