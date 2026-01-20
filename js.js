cfc5184b6fabda42547858829e58d1a451d2dfaf
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-01-20 17:25:43 +0100

    ,

--- js.js
+++ js.js
@@ -1,14 +1,14 @@
-// const inputValue = document.querySelector("#input").value;
-// const save = document.querySelector("#save");
-// const remove = document.querySelector("#delete");
+const inputValue = document.querySelector("#input").value;
+const save = document.querySelector("#save");
+const remove = document.querySelector("#delete");
 
-// save.addEventListener("click", () => {
-//   const inputValue = document.querySelector("#input").value;
-//   const text = localStorage.setItem("testo", inputValue);
-// });
-// remove.addEventListener("click", () => {
-//   localStorage.removeItem("testo");
-// });
+save.addEventListener("click", () => {
+  const inputValue = document.querySelector("#input").value;
+  const text = localStorage.setItem("testo", inputValue);
+});
+remove.addEventListener("click", () => {
+  localStorage.removeItem("testo");
+});
 window.addEventListener("load", () => {
   num = sessionStorage.getItem("number");
   number.textContent = num;
